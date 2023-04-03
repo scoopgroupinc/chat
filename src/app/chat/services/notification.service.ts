6f604cffcde9538e051d5db7cd5766c9f1b8e4ff
WeeTech Solution <weetech@users.noreply.github.com>
2023-04-03 10:56:39 +0000

    fix: update sqs message

--- src/app/chat/services/notification.service.ts
+++ src/app/chat/services/notification.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { INotification } from '../@types/INotification';
+import { Message } from 'sqs-producer';
 import { SQSProducer } from 'src/app/sqs/sqs-producer.service';
 
 @Injectable()
@@ -7,19 +8,22 @@ export class NotificationService {
   constructor(private producer: SQSProducer) {}
 
   async sendNotification(payload) {
-    const data: INotification = {
+    const sqsMessage: Message = {
       id: payload.id,
-      title: 'Message(s)',
-      body: `You have a new  from ${payload.name} `,
-      isRead: false,
-      image: null,
-      notificationType: 'chat',
-      userId: payload.senderID,
-      payload: Object({ receiverId: payload.receiverID }),
-      createdAt: new Date(),
+      body: JSON.stringify({
+        id: payload.id,
+        title: 'Message(s)',
+        body: `You have a new  from ${payload.name} `,
+        isRead: false,
+        image: null,
+        notificationType: 'chat',
+        userId: payload.senderID,
+        payload: Object({ receiverId: payload.receiverID }),
+        createdAt: new Date(),
+      } as INotification),
     };
 
-    await this.producer.produce(data);
+    await this.producer.produce(sqsMessage);
     return 'sent';
   }
 }
